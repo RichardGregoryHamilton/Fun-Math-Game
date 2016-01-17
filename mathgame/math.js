2a30defd3ca9678c8f706247145ecbcd0f12ed40
Richard Hamilton <richardgregoryhamilton@gmail.com>
2016-01-16 20:35:26 -0600

    lots of improvements

--- mathgame/math.js
+++ mathgame/math.js
@@ -1,13 +1,25 @@
 var gameBlock = document.getElementsByClassName("game-block")[0];
-var controls = document.getElementsByClassName("controls")[0];
 
 var gameStart = false;
 var score = 0;
 var level = 1;
-var scores = [0];
 
+var scores = [0];
+var achievements = [];
 var colors = ["red", "orange", "gold", "green", "lightblue"];
 var operators = ["+", "-", "*", "/"];
+
+/* This function stores a user's achievements over the length of a session */
+if (!localStorage["achievements"]) {
+    localStorage["achievements"] = JSON.stringify([]);
+}
+
+if (!localStorage["scores"]) {
+    localStorage["scores"] = JSON.stringify([]);
+}
+
+/* Hides achievement notifications at the start of games */
+$(".notification").css({ "visibility": "hidden" });
 
 // These functions return random numbers based on the level
 function randEasy() {
@@ -34,6 +46,13 @@ function randOperation() {
 function randColor() {
     return colors[Math.floor(Math.random() * colors.length)];
 }
+
+$(".example").each(function() {
+    $(this).css({ "color": "white",
+                  "background": randColor(),
+                  "text-align": "center",
+                  "padding": "8px 0" });
+});
 
 function generateExpressions() {
     var exp1 = document.createElement("div");
@@ -70,21 +89,9 @@ function generateExpressions() {
 
 // Creates the buttons
 function createButtons() {
-
-    var greaterThan = document.createElement("button");
-    greaterThan.classList.add("greaterThan");
-    var equals = document.createElement("button");
-    equals.classList.add("equals");
-    var lessThan = document.createElement("button");
-    lessThan.classList.add("lessThan");
-
-    greaterThan.innerHTML = ">";
-    equals.innerHTML = "=";
-    lessThan.innerHTML ="<";
-
-    controls.appendChild(greaterThan);
-    controls.appendChild(equals);
-    controls.appendChild(lessThan);
+    $(".controls").append("<button class='greaterThan'>" + ">" + "</button>");
+    $(".controls").append("<button class='equals'>=</button>")
+    $(".controls").append("<button class='lessThan'>" + "<" + "</button>")
 }
 
 // Update the score, remove expressions and generate new ones. Check for level increase
@@ -99,29 +106,99 @@ function incrementScore() {
 }
 
 function highScore() {
-    return Math.max.apply(Math,scores);
+    var userScores = JSON.parse(localStorage["scores"]);
+    var prevHighScore = Math.max.apply(Math, userScores);
+    var highScore = Math.max.apply(Math,scores);
+    return Math.max(prevHighScore, highScore);
+}
+
+// Updates the table rows in the achievements page
+function updateAchievements() {
+    var userAchievements = localStorage["achievements"];
+    var numbers = [];
+
+    for (var i = 0; i < userAchievements.length; i++) {
+        if (userAchievements[i].match(/\d/)) {
+            numbers.push(userAchievements[i]);
+        }
+    }
+
+    $("tr").each(function(index) {
+        var row = $(this);
+        var name = row.eq(index).first("td").html();
+        for (var i = 0; i < numbers.length; i++) {
+            if (name.match(numbers[i])) {
+                row.prop("class", "unlocked");
+            }
+            else {
+                row.prop("class", "locked");
+            }
+        }
+    });
+}
+
+// This function adds achievements indefinitely
+function addAchievement(i) {
+    var userAchievements = JSON.parse(localStorage["achievements"]);
+    var sessionAchievements = JSON.stringify(achievements);
+
+    if (userAchievements.length == (i - 1)) {
+        achievements.push("level" + i);
+        if (userAchievements.length > achievements.length) {
+            localStorage["achievements"] = localStorage["achievements"].replace("]", "") +
+            ("," + sessionAchievements.replace("[", ""));
+        }
+        else {
+            localStorage["achievements"] = sessionAchievements;
+        }
+        $(".notification").html("You have unlocked the achievement " + achievements[achievements.length - 1]);
+        $(".notification").css({ "visibility": "visible" });
+        setTimeout(function() {
+            $(".notification").css({ "visibility": "hidden" });
+        }, 5000);
+    }
+
+}
+
+function addScores() {
+    scores.push(score);
+    var userScores = JSON.parse(localStorage["scores"]);
+    var sessionScores = JSON.stringify(scores);
+    if (userScores.length > scores.length) {
+        localStorage["scores"] = localStorage["scores"].replace("]", "") +
+        ("," + sessionScores.replace("[", ""));
+    }
+    else {
+        localStorage["scores"] = sessionScores;;
+    }
 }
 
 // There are multiple conditionals so a switch statement works best here
 function incrementLevel() {
-     switch(true) {
-         case (score < 10):
-             level = 1;
-             break;
-         case (score >= 10 && score < 20):
-             level = 2;
-             break;
-         case (score >= 20 && score < 30):
-             level = 3;
-             break;
-         case (score >= 30 && score < 40):
-            level = 4;
+    switch(true) {
+        case (score < 10):
+            level = 1;
+            addAchievement(1);
             break;
-         case (score >= 40 && score < 50):
+        case (score >= 10 && score < 20):
+            level = 2;
+            addAchievement(2);
+            break;
+        case (score >= 20 && score < 30):
+            level = 3;
+            addAchievement(3);
+            break;
+        case (score >= 30 && score < 40):
+            level = 4;
+            addAchievement(4);
+            break;
+        case (score >= 40 && score < 50):
             level = 5;
+            addAchievement(5);
             break;
          case (score >= 50):
             level = 6;
+            addAchievement(6);
             break;
      }
      $("#level").html("<strong>Level: </strong>" + level + "<span id='score'>Score: " +
@@ -157,14 +234,24 @@ function startGame() {
         level = 1;
         generateExpressions();
         animateDivs();
-        $("#level").html("<strong>Level: </strong>" + level + "<span id='score'>Score: " + score + "</span><span id='high-score'> High Score: " + highScore() + "</span>");
+        $("#level").html("<strong>Level: </strong>" + level + "<span id='score'>Score: " +
+        score + "</span><span id='high-score'> High Score: " + highScore() + "</span>");
         createButtons();
     }
 }
 
+// Fill in the High Scores table
+$("#scores-table td:last-child").each(function(index) {
+    var userScores = JSON.parse(localStorage["scores"]).sort(function(a,b) {
+        return b - a;
+    });
+    $(this).html(userScores[index] || "");
+});
+
 function endGame() {
     $(".exp1, .exp2, .lessThan, .equals, .greaterThan").remove();
-    scores.push(score);
+    addScores();
+    updateAchievements();
     gameStart = false;
 }
 
